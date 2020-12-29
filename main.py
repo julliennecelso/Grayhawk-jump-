fe9156eb53cada7735cffc1bc339e3e91cacc8be
julliennecelso <76444898+julliennecelso@users.noreply.github.com>
2020-12-30 02:51:47 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -138,20 +138,20 @@ class MainApp(App):
         # Move the pipes
         #Clock.schedule_interval(self.move_pipes, 1/60.)
 
-    # def move_pipes(self, time_passed):
-    #     # Move pipes
-    #     for pipe in self.pipes:
-    #         pipe.x -= time_passed * 100
-    #
-    #     # Check if we need to reposition the pipe at the right side
-    #     num_pipes = 5
-    #     distance_between_pipes = Window.width / (num_pipes - 1)
-    #     pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
-    #     right_most_x = max(pipe_xs)
-    #     if right_most_x <= Window.width - distance_between_pipes:
-    #         most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
-    #         most_left_pipe.x = Window.width
-    #
+    def move_pipes(self, time_passed):
+        # Move pipes
+        for pipe in self.pipes:
+            pipe.x -= time_passed * 100
+
+        # Check if we need to reposition the pipe at the right side
+        num_pipes = 5
+        distance_between_pipes = Window.width / (num_pipes - 1)
+        pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
+        right_most_x = max(pipe_xs)
+        if right_most_x <= Window.width - distance_between_pipes:
+            most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
+            most_left_pipe.x = Window.width
+
 
 from kivymd.app import MDApp
 from kivy.lang import Builder
@@ -203,48 +203,5 @@ BoxLayout:
 '''
         )
 
-class Level1(Screen):
-    def move_pipes(self, time_passed):
-        for pipe in self.pipes:
-            pipe.x -= time_passed * 100
-
-        # Check if we need to reposition the pipe at the right side
-        num_pipes = 5
-        distance_between_pipes = Window.width / (num_pipes - 1)
-        pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
-        right_most_x = max(pipe_xs)
-        if right_most_x <= Window.width - distance_between_pipes:
-            most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
-            most_left_pipe.x = Window.width
-
-class Level2(Screen):
-    def move_pipes(self, time_passed):
-        for pipe in self.pipes:
-            pipe.x -= time_passed * 100
-
-        # Check if we need to reposition the pipe at the right side
-        num_pipes = 5
-        distance_between_pipes = Window.width / (num_pipes - 1)
-        pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
-        right_most_x = max(pipe_xs)
-        if right_most_x <= Window.width - distance_between_pipes:
-            most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
-            most_left_pipe.x = Window.width
-
-class Level3(Screen):
-    def move_pipes(self, time_passed):
-        for pipe in self.pipes:
-            pipe.x -= time_passed * 100
-
-        # Check if we need to reposition the pipe at the right side
-        num_pipes = 5
-        distance_between_pipes = Window.width / (num_pipes - 1)
-        pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
-        right_most_x = max(pipe_xs)
-        if right_most_x <= Window.width - distance_between_pipes:
-            most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
-            most_left_pipe.x = Window.width
-
 Test().run()
 MainApp().run()
-Level1().run()
